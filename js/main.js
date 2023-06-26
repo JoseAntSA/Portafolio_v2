5ee4b8f2d681e96acf4e28158a7c6cfba8c092a0
JoseAntSA <antoniosanchezalanis@gmail.com>
2023-06-26 16:55:57 -0600

    feat (section ‘Technology’)
    Se agrego código HTML y CSS de la section ‘Technology’.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.carousel-lista'), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    rewind: true,
+    dots: '.carousel-indicadores',
+    arrows: {
+      prev: '.carousel-prev',
+      next: '.carousel-next'
+    },
+    responsive: [{
+      // screens greater than >=768px
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }]
+  });
+});
+
 window.onload = function() {
   clickIconMenu();
   enableLink(); 
